927f6010c413c22d6ca18eb6d201cd69ea1be138
pazhevich <375292703311p@gmail.com>
2019-08-15 11:55:41 +0300

    folder 'out' added to .gitignore, created 'Hello world'

--- src/by/bsu/domino/controller/Controller.java
+++ src/by/bsu/domino/controller/Controller.java
@@ -1,7 +1,29 @@
 package by.bsu.domino.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/controller")
 public class Controller extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        this.processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        PrintWriter writer = resp.getWriter();
+        writer.write("hello world");
+    }
 }
